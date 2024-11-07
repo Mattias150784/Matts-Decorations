fdd9fe6694ae50f61a5a93cd8da3946b6a80a828
MattiasAye <Mattiasayenew@gmail.com>
2024-11-06 16:47:18 -0800

    Voxel Shape and Shadow weird

--- MultiLoader-Template-1.20.1/common/src/main/java/net/mattias/decorations/core/blocks/OakChair.java
+++ MultiLoader-Template-1.20.1/common/src/main/java/net/mattias/decorations/core/blocks/OakChair.java
@@ -18,7 +18,7 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 
 public class OakChair extends RotatedPillarBlock {
 
-    // Define the complete VoxelShape for the chair by calling makeShape()
+
     private static final VoxelShape CHAIR_SHAPE = makeShape();
 
     public OakChair(Properties properties) {
@@ -28,22 +28,21 @@ public class OakChair extends RotatedPillarBlock {
     private static VoxelShape makeShape() {
         VoxelShape shape = Shapes.empty();
 
-        // Legs
+
         shape = Shapes.join(shape, Shapes.box(0, 0, 0, 0.125, 0.5625, 0.125), BooleanOp.OR); // Front left leg
         shape = Shapes.join(shape, Shapes.box(0.875, 0, 0, 1, 0.5625, 0.125), BooleanOp.OR); // Front right leg
         shape = Shapes.join(shape, Shapes.box(0, 0, 0.875, 0.125, 0.5625, 1), BooleanOp.OR); // Back left leg
         shape = Shapes.join(shape, Shapes.box(0.875, 0, 0.875, 1, 0.5625, 1), BooleanOp.OR); // Back right leg
 
-        // Seat
+
         shape = Shapes.join(shape, Shapes.box(0, 0.5625, 0, 1, 0.6875, 1), BooleanOp.OR); // Seat area
 
-        // Backrest - extended to 1.6875 height
         shape = Shapes.join(shape, Shapes.box(0, 0.6875, 0.875, 1, 1.6875, 1), BooleanOp.OR); // Backrest
 
         return shape;
     }
 
-    // Override methods to use the custom VoxelShape
+
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
         return CHAIR_SHAPE;
